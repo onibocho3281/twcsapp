2fa3b3bcaa65270a2cfb22f2ca0a2b0e9bc5f8da
onibocho3281 <onibocho3281@gmail.com>
2025-10-17 23:17:18 +0300

    Add console log for Firebase config check
    Added a console log to check Firebase configuration.

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
+console.log("Firebase config check:", firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
